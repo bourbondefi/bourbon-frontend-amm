34276bec739ca697d7c02c2cf36b8d9346378de5
MCannoy <80276717+bourbondefi@users.noreply.github.com>
2021-03-09 03:24:53 -0500

    bourbonize

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -76,11 +76,11 @@ const config: MenuEntry[] = [
       },
     ],
   },
-  {
-    label: 'Partnerships/IFO',
-    icon: 'GooseIcon',
-    href: 'https://docs.google.com/forms/d/e/1FAIpQLSe7ycrw8Dq4C5Vjc9WNlRtTxEhFDB1Ny6jlAByZ2Y6qBo7SKg/viewform?usp=sf_link',
-  },
+ // {
+  //  label: 'Partnerships/IFO',
+ //   icon: 'GooseIcon',
+ //   href: 'https://docs.google.com/forms/d/e/1FAIpQLSe7ycrw8Dq4C5Vjc9WNlRtTxEhFDB1Ny6jlAByZ2Y6qBo7SKg/viewform?usp=sf_link',
+//  },
   {
     label: 'Audit by Hacken',
     icon: 'AuditIcon',
