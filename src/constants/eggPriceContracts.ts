ee54fe7014af67824c55fbdb9c8807d13c0c7ec0
bourbondefi <bourbondefi@gmail.com>
2021-03-18 19:57:18 -0400

    bourbonize

--- src/constants/eggPriceContracts.ts
+++ src/constants/eggPriceContracts.ts
@@ -1,5 +1,5 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
-  cakeAddress: '0x7DBaCb2Cca8e4870a64db85f4C91fc86D676E303',
+  cakeAddress: '0xcbe73dd7E8FC74011136b837a59205801c45e6A1',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
   lpAddress: '0x19e7cbecdd23a16dfa5573df54d98f7caae03019'
 }
