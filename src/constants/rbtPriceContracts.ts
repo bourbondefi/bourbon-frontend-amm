379ad43f6831f5cc9b378a66fa2793dd80313193
bourbondefi <bourbondefi@gmail.com>
2021-04-06 04:34:15 -0400

    Delete rbtPriceContracts.ts

--- src/constants/rbtPriceContracts.ts
+++
@@ -1,7 +0,0 @@
-const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
-  cakeAddress: '0xcbe73dd7E8FC74011136b837a59205801c45e6A1',
-  busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-  lpAddress: '0x1b30e60130b364DDfC0267525d7D8039d795FB33'
-}
-
-export default priceContracts
